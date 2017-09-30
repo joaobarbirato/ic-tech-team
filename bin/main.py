ddc6241c752ff97b612f1491b20a4f89907f6574
renatasarmet <renatassmendes@hotmail.com>
2017-09-30 16:41:24 -0300

    Stopwords implementation

--- bin/main.py
+++ bin/main.py
@@ -1,13 +1,20 @@
+# -*- coding: utf-8 -*-
 import sys
 sys.path.append('./../source')
 
+from NoStopWords import NoStopWords
 #call everything and do stuff
 import Bag
 
 bag = Bag.Bag()
 
 rawText = open("./../data/raw-text2.txt", "rt")
-words = rawText.read().split(' ')
+
+words = rawText.read()
+words = words.lower()
+
+words = NoStopWords(words)
+words = words.split(' ')
 
 for w in words:
     if(bag.isInTheBag(w)):
